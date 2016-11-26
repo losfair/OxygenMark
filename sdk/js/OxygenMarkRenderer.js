e3741e506d127d91febe634befa997101e34ee09
losfair <zhy20000919@hotmail.com>
2016-11-26 17:36:49 +0800

    Fix Javascript SDK

--- sdk/js/OxygenMarkRenderer.js
+++ sdk/js/OxygenMarkRenderer.js
@@ -1,8 +1,8 @@
 var OxygenMarkLib = require("./OxygenMarkBundle.min.js");
 
 function OxygenMark() {
-    this.prototype.loadDocumentFromSource = OxygenMarkLib.cwrap("loadDocumentFromSource", "number", ["string"]);
-    this.prototype.setDocumentParam = OxygenMarkLib.cwrap("setDocumentParam", null, ["number", "string", "string"]);
-    this.prototype.renderToHtml = OxygenMarkLib.cwrap("renderToHtml", "string", ["number", "number"]);
-    this.prototype.destroyDocument = OxygenMarkLib.cwrap("destroyDocument", null, ["number"]);
+    this.loadDocumentFromSource = OxygenMarkLib.cwrap("loadDocumentFromSource", "number", ["string"]);
+    this.setDocumentParam = OxygenMarkLib.cwrap("setDocumentParam", null, ["number", "string", "string"]);
+    this.renderToHtml = OxygenMarkLib.cwrap("renderToHtml", "string", ["number", "number"]);
+    this.destroyDocument = OxygenMarkLib.cwrap("destroyDocument", null, ["number"]);
 }
